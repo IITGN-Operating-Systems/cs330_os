88ae7eecb0b38e7759439a99dcd844bcfa271cd1
Pro3757 <pratyush3757@gmail.com>
2025-03-24 01:10:16 +0530

    add lab5 template

--- kern/src/main.rs
+++ kern/src/main.rs
@@ -8,16 +8,29 @@
 #[cfg(not(test))]
 mod init;
 
+extern crate alloc;
+
+pub mod allocator;
 pub mod console;
+pub mod fs;
 pub mod mutex;
 pub mod shell;
 
 use console::kprintln;
 
-// FIXME: You need to add dependencies here to
-// test your drivers (Phase 2). Add them as needed.
+use allocator::Allocator;
+use fs::FileSystem;
+
+#[cfg_attr(not(test), global_allocator)]
+pub static ALLOCATOR: Allocator = Allocator::uninitialized();
+pub static FILESYSTEM: FileSystem = FileSystem::uninitialized();
 
 fn kmain() -> ! {
-    // FIXME: Start the shell.
-    unimplemented!()
+    unsafe {
+        ALLOCATOR.initialize();
+        FILESYSTEM.initialize();
+    }
+
+    kprintln!("Welcome to cs330!");
+    shell::shell("> ");
 }
